bae7f2427fb17823519d45702d6fe26441b107a3
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-10-15 14:25:25 +0100

    created header file for Value class

--- value.hpp
+++ value.hpp
@@ -13,84 +13,59 @@ class Value {
 public:
   double data;
   double grad;
-  std::vector<Value*> prev;
+  std::set<Value*> prev;
   std::string op;
+  std::function<void()> _backward;
 
-  Value(double data, const std::vector<Value*>& children = {}, const std::string& op = "")
-      : data(data), grad(0), prev(children), op(op) {}
+  explicit Value(double data, const std::set<Value*>& children = {}, std::string op = "")
+      : data(data), grad(0), prev(children), op(std::move(op)) {}
 
-  Value operator+(const Value& other) const {
-    return Value(data + other.data, {const_cast<Value*>(this), const_cast<Value*>(&other)}, "+");
-  }
+  Value operator+(Value other) {
+    /*
+    we create a new Value object to return e.g. for v = v1 + v2
+    const_cast casts 'this' to a pointer to Value, so it can be added to the set previous values
+    we then set _backward to a lambda function that captures 'other', 'this' and 'out'
+    and sets the grads of 'this' and 'other'
 
-  Value operator*(const Value& other) const {
-    return Value(data * other.data, {const_cast<Value*>(this), const_cast<Value*>(&other)}, "*");
-  }
+      z = x + y
+      dz/dx = 1, dz/dy = 1
+      df(z)/dx = (df/dz)(dz/dx) = (df/dz) * 1 = df/dz
 
-  Value operator-(const Value& other) const {
-    return Value(data - other.data, {const_cast<Value*>(this), const_cast<Value*>(&other)}, "-");
-  }
+      hence, 'this'-> grad = out->grad
+      same applied for 'other'
+    */
 
-  Value pow(double power) const {
-    double result = std::pow(data, power);
-    return Value(result, {const_cast<Value*>(this)}, "**" + std::to_string(power));
-  }
-
-  Value tanh() const {
-    double t = std::tanh(data);
-    return Value(t, {const_cast<Value*>(this)}, "tanh");
-  }
-
-  void backward() {
-    std::vector<Value*> topo;
-    std::set<Value*> visited;
-
-    std::function<void(Value*)> buildTopo = [&](Value* root) {
-      if (visited.find(root) == visited.end()) {
-        visited.insert(root);
-        for (Value* child : root->prev) {
-          buildTopo(child);
-        }
-        topo.push_back(root);
-      }
+    auto out = Value(data + other.data, {const_cast<Value*>(this), &other}, "+");
+    out._backward = [&other, &out, this]() {
+      this->grad += out.grad;
+      other.grad += out.grad;
     };
 
-    buildTopo(this);
-    grad = 1.0;
-    for (auto it = topo.rbegin(); it != topo.rend(); ++it) {
-      Value* node = *it;
-      if (node->op == "+") {
-        node->prev[0]->grad += grad;
-        node->prev[1]->grad += grad;
-      } else if (node->op == "*") {
-        node->prev[0]->grad += node->prev[1]->data * grad;
-        node->prev[1]->grad += node->prev[0]->data * grad;
-      } else if (node->op == "-") {
-        node->prev[0]->grad -= grad;
-      } else if (node->op.substr(0, 2) == "**") {
-        double power = std::stod(node->op.substr(2));
-        node->prev[0]->grad += power * std::pow(node->prev[0]->data, power - 1) * grad;
-      } else if (node->op == "tanh") {
-        double t = std::tanh(node->data);
-        node->prev[0]->grad += (1 - t * t) * grad;
-      }
-    }
+    return out;
+  }
+
+  void operator-() { this->data = -(this->data); }
+
+  Value operator*(Value other) {
+    /*
+      z = x * y
+      dz/dx = y, dz/dy = x
+      df(z)/dx = (df/dz)(dz/dx) = (df/dz) * y = out.grad * other.data
+      df(z)/dy = (df/dz)(dz/dy) = (df/dz) * x = out.grad * this.data
+    */
+    auto out = Value(data * other.data, {const_cast<Value*>(this), &other}, "*");
+    out._backward = [&other, &out, this]() {
+      this->grad += other.data * out.grad;
+      other.grad += this->data * out.grad;
+    };
+
+    return out;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Value& value) {
+    os << "Value(data: " << value.data << ", grad: " << value.grad << ")";
+    return os;
   }
 };
-
-int main() {
-  Value x(2.0);
-  Value y(3.0);
-  Value z = x * y + x.pow(2) - y;
-
-  // Perform backward propagation
-  z.backward();
-
-  std::cout << "x.grad: " << x.grad << std::endl; // Output: 7.62149
-  std::cout << "y.grad: " << y.grad << std::endl; // Output: -1.07077
-
-  return 0;
-}
-
 
 #endif //MICROGRAD_VALUE_HPP
