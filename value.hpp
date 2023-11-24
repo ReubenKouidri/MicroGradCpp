f014b24dfe03b657d21b51dc2ff61942f423ab49
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-11-24 21:30:31 +0000

    added operator/ overloads, friend pow func

--- value.hpp
+++ value.hpp
@@ -12,8 +12,8 @@
 inline std::function<void()> do_nothing = [](){};
 
 class Value {
-  double _data;
-  double _grad {0};
+  double _data {0.0};
+  double _grad {0.0};
   std::set<std::shared_ptr<Value>> _parents;
   std::string op;
   std::function<void()> _backward = do_nothing;
@@ -22,32 +22,40 @@ class Value {
 public:
   explicit Value(const double data, const std::set<std::shared_ptr<Value>>& parents, std::string op = "")
       : _data(data), _parents(parents), op(std::move(op)) {}
-  explicit Value(const double data) : _data(data) {}
-  Value(): _data(0) { _ptr = std::make_shared<Value>(_data); }
+  Value(const double data, const std::set<std::shared_ptr<Value>>& parents): _data{data}, _parents{parents} {
+    op = "";
+  }
   ~Value() = default;
-  Value(const double data, const std::set<std::shared_ptr<Value>>& parents): _data{data}, _parents{parents}
-  {}
 
-  [[nodiscard]] double get_data() const { return _data; }
-  [[nodiscard]] double get_grad() const {return _grad; }
-  [[nodiscard]] std::shared_ptr<Value> get_ptr() { return _ptr; }
+  friend std::ostream& operator<<(std::ostream& os, const Value& value) {
+    os << "Value(_data: " << value._data << ", grad: " << value.get_grad() << ")";
+    return os;
+  }
+
+  friend Value pow(const Value& val, const int exp) {
+    auto out = Value(std::pow(val.get_data(), exp), {val.get_ptr(), }, "**");
+
+    const Value* val_ptr = val.get_ptr().get();
+    const Value* out_ptr = out.get_ptr().get();
+
+    auto _back = [=]() {
+      val_ptr->get_grad() += (exp * std::pow(val_ptr->get_data(), exp - 1)) * out_ptr->get_grad();
+    };
+    out.set_backward(_back);
+
+    return out;
+  }
+
+  [[nodiscard]] const double& get_data() const { return _data; }
+  [[nodiscard]] const double& get_grad() const {return _grad; }
+  [[nodiscard]] double& get_data() { return _data; }
+  [[nodiscard]] double& get_grad() { return _grad; }
+
+
+  [[nodiscard]] std::shared_ptr<Value> get_ptr() const { return _ptr; }
   void set_backward(const std::function<void()>& func) { _backward = func; }
 
-  Value operator+(Value& other) {
-    /*
-    we create a new Value object to return e.g. for v = v1 + v2
-    const_cast casts 'this' to a pointer to Value, so it can be added to the set _parents values
-    we then set _backward to a lambda function that captures 'other', 'this' and 'out'
-    and sets the grads of 'this' and 'other'
-
-      z = x + y
-      dz/dx = 1, dz/dy = 1
-      df(z)/dx = (df/dz)(dz/dx) = (df/dz) * 1 = df/dz
-
-      hence, 'this'-> _grad = out->_grad
-      same applied for 'other'
-    */
-
+  Value operator+(const Value& other) const {
     auto out = Value(
       get_data() + other.get_data(),
       {get_ptr(), other.get_ptr()},
@@ -70,13 +78,7 @@ public:
 
   void operator-() { this->_data = -(this->_data); }
 
-  Value operator*(Value other) {
-    /*
-      z = x * y
-      dz/dx = y, dz/dy = x
-      df(z)/dx = (df/dz)(dz/dx) = (df/dz) * y = out._grad * other._data
-      df(z)/dy = (df/dz)(dz/dy) = (df/dz) * x = out._grad * this._data
-    */
+  Value operator*(const Value& other) const {
 
     auto out = Value(
       get_data() * other.get_data(),
@@ -96,9 +98,11 @@ public:
     return out;
   }
 
-  friend std::ostream& operator<<(std::ostream& os, const Value& value) {
-    os << "Value(_data: " << value._data << ", grad: " << value.get_grad() << ")";
-    return os;
+  void operator/(const double denom) { _data /= denom; }
+
+  Value operator/(const Value& other) const {
+    const auto temp = pow(other, -1);
+    return operator*(temp);
   }
 };
 
