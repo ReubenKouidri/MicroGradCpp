8b1b46432f125a0526300f25683d5f1c70ac81d2
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-11-24 20:35:20 +0000

    parents now shared_ptr's, name changes, lambda capture by value instead ref, helper funcs

--- value.hpp
+++ value.hpp
@@ -4,26 +4,39 @@
 
 #include <iostream>
 #include <cmath>
+#include <utility>
 #include <vector>
 #include <set>
 #include <functional>
 
+inline std::function<void()> do_nothing = [](){};
 
 class Value {
-public:
-  double data;
-  double grad;
-  std::set<Value*> prev;
+  double _data;
+  double _grad {0};
+  std::set<std::shared_ptr<Value>> _parents;
   std::string op;
-  std::function<void()> _backward;
+  std::function<void()> _backward = do_nothing;
+  std::shared_ptr<Value> _ptr = std::make_shared<*this>;
 
-  explicit Value(double data, const std::set<Value*>& children = {}, std::string op = "")
-      : data(data), grad(0), prev(children), op(std::move(op)) {}
+public:
+  explicit Value(const double data, const std::set<std::shared_ptr<Value>>& parents, std::string op = "")
+      : _data(data), _parents(parents), op(std::move(op)) {}
+  explicit Value(const double data) : _data(data) {}
+  Value(): _data(0) { _ptr = std::make_shared<Value>(_data); }
+  ~Value() = default;
+  Value(const double data, const std::set<std::shared_ptr<Value>>& parents): _data{data}, _parents{parents}
+  {}
 
-  Value operator+(Value other) {
+  [[nodiscard]] double get_data() const { return _data; }
+  [[nodiscard]] double get_grad() const {return _grad; }
+  [[nodiscard]] std::shared_ptr<Value> get_ptr() { return _ptr; }
+  void set_backward(const std::function<void()>& func) { _backward = func; }
+
+  Value operator+(Value& other) {
     /*
     we create a new Value object to return e.g. for v = v1 + v2
-    const_cast casts 'this' to a pointer to Value, so it can be added to the set previous values
+    const_cast casts 'this' to a pointer to Value, so it can be added to the set _parents values
     we then set _backward to a lambda function that captures 'other', 'this' and 'out'
     and sets the grads of 'this' and 'other'
 
@@ -31,39 +44,60 @@ public:
       dz/dx = 1, dz/dy = 1
       df(z)/dx = (df/dz)(dz/dx) = (df/dz) * 1 = df/dz
 
-      hence, 'this'-> grad = out->grad
+      hence, 'this'-> _grad = out->_grad
       same applied for 'other'
     */
 
-    auto out = Value(data + other.data, {const_cast<Value*>(this), &other}, "+");
-    out._backward = [&other, &out, this]() {
-      this->grad += out.grad;
-      other.grad += out.grad;
+    auto out = Value(
+      get_data() + other.get_data(),
+      {get_ptr(), other.get_ptr()},
+      "+");
+
+    Value* const other_ptr = other.get_ptr().get();
+    Value* const this_ptr = get_ptr().get();
+    const Value* const out_ptr = out.get_ptr().get();
+    // When a new Value is created by addition, the backward function is registered capturing these three pointers
+    // by value. When _backward is called, it'll operate on these.
+    // Capture by ref leaves dangling references
+    auto _back = [=]() {
+      this_ptr->_grad += out_ptr->_grad;
+      other_ptr->_grad += out_ptr->_grad;
     };
+    out.set_backward(_back);
 
     return out;
   }
 
-  void operator-() { this->data = -(this->data); }
+  void operator-() { this->_data = -(this->_data); }
 
   Value operator*(Value other) {
     /*
       z = x * y
       dz/dx = y, dz/dy = x
-      df(z)/dx = (df/dz)(dz/dx) = (df/dz) * y = out.grad * other.data
-      df(z)/dy = (df/dz)(dz/dy) = (df/dz) * x = out.grad * this.data
+      df(z)/dx = (df/dz)(dz/dx) = (df/dz) * y = out._grad * other._data
+      df(z)/dy = (df/dz)(dz/dy) = (df/dz) * x = out._grad * this._data
     */
-    auto out = Value(data * other.data, {const_cast<Value*>(this), &other}, "*");
-    out._backward = [&other, &out, this]() {
-      this->grad += other.data * out.grad;
-      other.grad += this->data * out.grad;
+
+    auto out = Value(
+      get_data() * other.get_data(),
+      {get_ptr(), other.get_ptr()},
+      "*");
+
+    Value* const this_ptr = get_ptr().get();
+    Value* const other_ptr = other.get_ptr().get();
+    const Value* const out_ptr = out.get_ptr().get();
+
+    auto _back = [=]() {
+      this_ptr->_grad += other_ptr->_data * out_ptr->_grad;
+      other_ptr->_grad += this_ptr->_data * out_ptr->_grad;
     };
 
+    out.set_backward(_back);
     return out;
   }
 
   friend std::ostream& operator<<(std::ostream& os, const Value& value) {
-    os << "Value(data: " << value.data << ", grad: " << value.grad << ")";
+    os << "Value(_data: " << value._data << ", grad: " << value.get_grad() << ")";
     return os;
   }
 };
