39e7afe2e15b535a0f7b24fea46fbc1643c475d9
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-11-25 18:24:24 +0000

    failing test: shared_ptrs not tracking original pointers?

--- value.hpp
+++ value.hpp
@@ -4,10 +4,12 @@
 
 #include <iostream>
 #include <cmath>
+#include <ranges>
 #include <utility>
 #include <vector>
 #include <set>
 #include <functional>
+// ts: 18:14 to return to
 
 inline std::function<void()> do_nothing = [](){};
 
@@ -17,14 +19,12 @@ class Value {
   std::set<std::shared_ptr<Value>> _parents;
   std::string op;
   std::function<void()> _backward = do_nothing;
-  std::shared_ptr<Value> _ptr = std::make_shared<*this>;
 
 public:
   explicit Value(const double data, const std::set<std::shared_ptr<Value>>& parents, std::string op = "")
       : _data(data), _parents(parents), op(std::move(op)) {}
-  Value(const double data, const std::set<std::shared_ptr<Value>>& parents): _data{data}, _parents{parents} {
-    op = "";
-  }
+  explicit Value(const double data): _data(data) {}
+  Value(const double data, const std::set<std::shared_ptr<Value>>& parents): _data{data}, _parents{parents} { op = ""; }
   ~Value() = default;
 
   friend std::ostream& operator<<(std::ostream& os, const Value& value) {
@@ -32,77 +32,113 @@ public:
     return os;
   }
 
-  friend Value pow(const Value& val, const int exp) {
-    auto out = Value(std::pow(val.get_data(), exp), {val.get_ptr(), }, "**");
-
-    const Value* val_ptr = val.get_ptr().get();
-    const Value* out_ptr = out.get_ptr().get();
-
-    auto _back = [=]() {
-      val_ptr->get_grad() += (exp * std::pow(val_ptr->get_data(), exp - 1)) * out_ptr->get_grad();
-    };
-    out.set_backward(_back);
-
-    return out;
+  [[nodiscard]] const double& get_data() const { return _data; }
+  [[nodiscard]] double& get_data() {return _data; }
+  void set_data(const double new_val) { _data = new_val; }
+  void set_grad(const double new_val) { _grad = new_val; }
+  [[nodiscard]] const double& get_grad() const {return _grad; }
+  [[nodiscard]] double& get_grad() { return _grad; }
+  void zero_grad() { _grad = 0.0; }
+  void zero_grad_all() {
+    const auto order = build_topological_order();
+    for (const auto& n : std::ranges::reverse_view(order)) { n->zero_grad(); }
   }
 
-  [[nodiscard]] const double& get_data() const { return _data; }
-  [[nodiscard]] const double& get_grad() const {return _grad; }
-  [[nodiscard]] double& get_data() { return _data; }
-  [[nodiscard]] double& get_grad() { return _grad; }
-
-
-  [[nodiscard]] std::shared_ptr<Value> get_ptr() const { return _ptr; }
+  [[nodiscard]] const std::set<std::shared_ptr<Value>>& get_parents() const { return _parents; }
   void set_backward(const std::function<void()>& func) { _backward = func; }
 
-  Value operator+(const Value& other) const {
-    auto out = Value(
-      get_data() + other.get_data(),
-      {get_ptr(), other.get_ptr()},
-      "+");
+  Value operator+(Value& other) {
+    auto out = Value(get_data() + other.get_data(),
+    {std::make_shared<Value>(*this), std::make_shared<Value>(other)}, "+");
 
-    Value* const other_ptr = other.get_ptr().get();
-    Value* const this_ptr = get_ptr().get();
-    const Value* const out_ptr = out.get_ptr().get();
-    // When a new Value is created by addition, the backward function is registered capturing these three pointers
-    // by value. When _backward is called, it'll operate on these.
-    // Capture by ref leaves dangling references
-    auto _back = [=]() {
-      this_ptr->_grad += out_ptr->_grad;
+    auto out_ptr = &out;
+    auto other_ptr = &other;
+
+    auto _back = [this, out_ptr, other_ptr]() mutable {
+      this->_grad += out_ptr->_grad;
       other_ptr->_grad += out_ptr->_grad;
     };
     out.set_backward(_back);
-
     return out;
   }
 
-  void operator-() { this->_data = -(this->_data); }
+  Value operator+(const double other) {
+    auto temp = Value(other);
+    return operator+(temp);
+  }
 
-  Value operator*(const Value& other) const {
+  void operator-() { _data = -_data; }
+
+  Value operator*(Value& other) {
 
     auto out = Value(
       get_data() * other.get_data(),
-      {get_ptr(), other.get_ptr()},
+      {std::make_shared<Value>(*this), std::make_shared<Value>(other)},
       "*");
 
-    Value* const this_ptr = get_ptr().get();
-    Value* const other_ptr = other.get_ptr().get();
-    const Value* const out_ptr = out.get_ptr().get();
+    // Value* const this_ptr = get_ptr().get();
+    // Value* const other_ptr = other.get_ptr().get();
+    // const Value* const out_ptr = out.get_ptr().get();
+    auto out_ptr = &out;
+    auto other_ptr = &other;
 
-    auto _back = [=]() {
-      this_ptr->_grad += other_ptr->_data * out_ptr->_grad;
-      other_ptr->_grad += this_ptr->_data * out_ptr->_grad;
+    auto _back = [this, out_ptr, other_ptr]() mutable {
+      this->_grad += other_ptr->_data * out_ptr->_grad;
+      other_ptr->_grad += this->_data * out_ptr->_grad;
     };
 
     out.set_backward(_back);
     return out;
   }
 
-  void operator/(const double denom) { _data /= denom; }
+  Value operator*(const double other) {
+    auto temp = Value(other);
+    return operator*(temp);
+  }
 
+
+
+  /*
   Value operator/(const Value& other) const {
     const auto temp = pow(other, -1);
     return operator*(temp);
+  }
+
+  Value operator/(const double other) const {
+    const auto temp = Value(other);
+    return pow(temp, -1);
+  }
+  */
+
+  std::vector<Value*> build_topological_order() {
+
+    std::vector<Value*> topological_order;
+    std::set<Value*> visited_nodes;
+    // declare lambda
+    std::function<void(Value*, std::set<Value*>&, std::vector<Value*>&)> traverse_and_build_order;
+    traverse_and_build_order = [&traverse_and_build_order](
+      Value* node, std::set<Value*>& visited, std::vector<Value*>& order)
+    {
+      if (visited.contains(node))
+        return;
+
+      visited.insert(node);
+      for (const auto& parent_ptr : node->get_parents())
+        traverse_and_build_order(parent_ptr.get(), visited, order);
+      order.push_back(node);
+    };
+
+    traverse_and_build_order(this, visited_nodes, topological_order);
+    return topological_order;
+  }
+
+  void backward() {
+    auto topological_order = build_topological_order();
+    _grad = 1.0; // Set dx/dx=1
+    for (const auto& node : std::ranges::reverse_view(topological_order)) {
+      if (node != nullptr) node->_backward();
+      else { return; }
+    }
   }
 };
 
