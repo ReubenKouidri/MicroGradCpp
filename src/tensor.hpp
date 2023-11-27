2e842be383bb2f9c569f2e30bae0708df3144958
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-11-27 12:13:34 +0000

    added activation output function

--- src/tensor.hpp
+++ src/tensor.hpp
@@ -19,35 +19,35 @@ class BaseTensor {
     return os;
   }
 
-  T _data { static_cast<T>(0) };
-  T _grad { static_cast<T>(0) };
-  std::set<std::shared_ptr<BaseTensor>> _parents;
-  std::function<void()> _backward { do_nothing };
+  T data_ { static_cast<T>(0) };
+  T grad_ { static_cast<T>(0) };
+  std::set<std::shared_ptr<BaseTensor>> parents_;
+  std::function<void()> backward_ { do_nothing };
 
 public:
   BaseTensor(const T& data, const std::set<std::shared_ptr<BaseTensor>>& parents):
-    _data{data}, _parents{parents} {}
-  explicit BaseTensor(const T& data): _data(data) {}
+    data_{data}, parents_{parents} {}
+  explicit BaseTensor(const T& data): data_(data) {}
   ~BaseTensor() = default;
   BaseTensor(const BaseTensor&) = delete;
   BaseTensor(BaseTensor&&) = delete;
   BaseTensor& operator=(const BaseTensor& other) = delete;
   BaseTensor& operator=(BaseTensor&& other) = delete;
 
-  const T& get_data() const { return _data; }
-  const T& get_grad() const { return _grad; }
-  T& get_data() { return _data; }
-  T& get_grad() { return _grad; }
-  const std::set<std::shared_ptr<BaseTensor>>& get_parent_ptrs() const { return _parents; }
-  void zero_grad() { _grad = static_cast<T>(0); }
+  const T& get_data() const { return data_; }
+  const T& get_grad() const { return grad_; }
+  T& get_data() { return data_; }
+  T& get_grad() { return grad_; }
+  const std::set<std::shared_ptr<BaseTensor>>& get_parent_ptrs() const { return parents_; }
+  void zero_grad() { grad_ = static_cast<T>(0); }
   void zero_grad_all() {
     const auto order = build_topological_order();
     for (auto n = order.rbegin(); n != order.rend(); ++n)
       (*n)->zero_grad();
   }
 
-  void set_backward(const std::function<void()>& func) { _backward = func; }
-  void step(const T& learning_rate) { _data -= learning_rate * _grad; }
+  void set_backward(const std::function<void()>& func) { backward_ = func; }
+  void step(const T& learning_rate) { data_ -= learning_rate * grad_; }
 
   std::vector<BaseTensor*> build_topological_order() {
     std::vector<BaseTensor*> topological_order;
@@ -70,17 +70,19 @@ public:
 
   void backward() {
     const auto topological_order = build_topological_order();
-    _grad = static_cast<T>(1); // Set dx/dx=1
+    grad_ = static_cast<T>(1); // Set dx/dx=1
     for (const auto& node: std::ranges::reverse_view(topological_order)) {
-      if (node != nullptr) node->_backward();
+      if (node != nullptr) node->backward_();
       else { return; }
     }
   }
 };
 
+using namespace gradient_ops;
 
 template<typename T>
 class Tensor {
+
   template<typename C> friend std::ostream& operator<<(std::ostream& os, const Tensor<C>& val) {
     os << "Tensor(" << val.get_data() << ", " << val.get_grad() << ")";
     return os;
@@ -93,40 +95,43 @@ class Tensor {
   template<class C> friend Tensor<C> operator/(C num, const Tensor<C>& val) { return val / num; }
 
   Tensor(const T& data, const std::set<std::shared_ptr<BaseTensor<T>>>& parents) {
-    _ptr = std::make_shared<BaseTensor<T>>(data, parents);
+    ptr_ = std::make_shared<BaseTensor<T>>(data, parents);
   }
-  // _ptr contains the data. This class just manages it.
-  std::shared_ptr<BaseTensor<T>> _ptr = nullptr;
+  // ptr_ contains the data. This class just manages it.
+  std::shared_ptr<BaseTensor<T>> ptr_ = nullptr;
 
 public:
-  Tensor() {_ptr = std::make_shared<BaseTensor<T>>(static_cast<T>(0)); }
-  explicit Tensor(const T& data) { _ptr = std::make_shared<BaseTensor<T>>(data); }
-  ~Tensor() { _ptr = nullptr; }
-  Tensor(const Tensor& other) { _ptr = other._ptr; }
+  Tensor() { ptr_ = std::make_shared<BaseTensor<T>>(static_cast<T>(0)); }
+  explicit Tensor(const T& data, const Activation& act) { ptr_ = std::make_shared<BaseTensor<T>>(data);}
+  explicit Tensor(const T& data) { ptr_ = std::make_shared<BaseTensor<T>>(data); }
+  ~Tensor() { ptr_ = nullptr; }
+  Tensor(const Tensor& other) { ptr_ = other.ptr_; }
   Tensor(Tensor&& other) noexcept {
-    _ptr = other._ptr;
-    other._ptr = nullptr;
+    ptr_ = other.ptr_;
+    other.ptr_ = nullptr;
   }
   Tensor& operator=(const Tensor& other) {
-    if (&other != this) _ptr = other._ptr;
+    if (&other != this) {
+      ptr_ = other.ptr_;
+    }
     return *this;
   }
   Tensor& operator=(Tensor&& other) noexcept {
-    _ptr = other._ptr;
-    other._ptr = nullptr;
+    ptr_ = other.ptr_;
+    other.ptr_ = nullptr;
     return *this;
   }
 
-  std::shared_ptr<BaseTensor<T>> get_ptr() const { return _ptr; }
-  void set_backward(std::function<void()> func) const { _ptr->set_backward(func); }
-  void backward() const { _ptr->backward(); }
-  void zero_grad() { _ptr->_grad = static_cast<T>(0); }
-  void zero_grad_all() { _ptr->zero_grad_all(); }
-  const T& get_data() const { return _ptr->get_data(); }
-  const T& get_grad() const { return _ptr->get_grad(); }
-  T& get_data() { return _ptr->get_data(); }
-  T& get_grad() { return _ptr->get_grad(); }
-  std::vector<BaseTensor<T>*> build_topo() const { return _ptr->build_topological_order(); }
+  std::shared_ptr<BaseTensor<T>> get_ptr() const { return ptr_; }
+  void set_backward(std::function<void()> func) const { ptr_->set_backward(func); }
+  void backward() const { ptr_->backward(); }
+  void zero_grad() { ptr_->grad_ = static_cast<T>(0); }
+  void zero_grad_all() { ptr_->zero_grad_all(); }
+  const T& get_data() const { return ptr_->get_data(); }
+  const T& get_grad() const { return ptr_->get_grad(); }
+  T& get_data() { return ptr_->get_data(); }
+  T& get_grad() { return ptr_->get_grad(); }
+  std::vector<BaseTensor<T>*> build_topo() const { return ptr_->build_topological_order(); }
 
   Tensor operator+(const Tensor& other) const {
     Tensor out(
@@ -137,7 +142,7 @@ public:
     BaseTensor<T>* this_ptr = get_ptr().get();
     BaseTensor<T>* other_ptr = other.get_ptr().get();
     BaseTensor<T>* out_ptr = out.get_ptr().get();
-    RegisterGradient<T>::register_backward(this_ptr, other_ptr, out_ptr, RegisterGradient<T>::Operation::ADD);
+    RegisterGradient<T>::register_backward(this_ptr, other_ptr, out_ptr, Operation::ADD);
     return out;
   }
   Tensor operator+(const T& other) const {
@@ -153,7 +158,7 @@ public:
     BaseTensor<T>* this_ptr = get_ptr().get();
     BaseTensor<T>* other_ptr = other.get_ptr().get();
     BaseTensor<T>* out_ptr = out.get_ptr().get();
-    RegisterGradient<T>::register_backward(this_ptr, other_ptr, out_ptr, RegisterGradient<T>::Operation::SUBTRACT);
+    RegisterGradient<T>::register_backward(this_ptr, other_ptr, out_ptr, Operation::SUBTRACT);
     return out;
   }
   Tensor operator-(const T& other) const {
@@ -166,7 +171,7 @@ public:
     BaseTensor<T>* this_ptr = get_ptr().get();
     BaseTensor<T>* other_ptr = other.get_ptr().get();
     BaseTensor<T>* out_ptr = out.get_ptr().get();
-    RegisterGradient<T>::register_backward(this_ptr, other_ptr, out_ptr, RegisterGradient<T>::Operation::DIVIDE);
+    RegisterGradient<T>::register_backward(this_ptr, other_ptr, out_ptr, Operation::DIVIDE);
     return out;
   }
   Tensor operator/(const T& other) const {
@@ -180,7 +185,7 @@ public:
     BaseTensor<T>* this_ptr = get_ptr().get();
     BaseTensor<T>* other_ptr = other.get_ptr().get();
     BaseTensor<T>* out_ptr = out.get_ptr().get();
-    RegisterGradient<T>::register_backward(this_ptr, other_ptr, out_ptr, RegisterGradient<T>::Operation::MULTIPLY);
+    RegisterGradient<T>::register_backward(this_ptr, other_ptr, out_ptr, Operation::MULTIPLY);
     return out;
   }
   Tensor operator*(const T& other) const {
@@ -188,7 +193,17 @@ public:
     return operator*(temp);
   }
   void operator-() {
-    _ptr->get_data() = -_ptr->get_data();
+    ptr_->get_data() = -ptr_->get_data();
+  }
+
+  Tensor activation_output(const Activation& act) const {
+    auto output_data = ActivationOutput<T>::func(get_data(), act);
+    auto output_tensor = Tensor(output_data, {get_ptr()});
+
+    BaseTensor<T>* out_ptr = output_tensor.get_ptr().get();
+    RegisterGradient<T>::register_backward(get_ptr().get(), out_ptr, act);
+
+    return output_tensor;
   }
 };
 
