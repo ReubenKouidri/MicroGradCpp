ce750fddc5a5364c6fe57f52c4c4a455002909b5
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-11-28 11:03:15 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-#include "src/tensor.hpp"
+#include "src/module.hpp"
 
 int main() {
+  constexpr size_t nin = 3;
+  const std::vector<size_t> neurons {4, 4, 2};
+  const std::vector<double> inputs {1.0, 1.5, 2.0};
+  const std::vector<double> targets {1.0, 0.0};
+  auto mlp = MLP<double>(nin, neurons);
+  auto mse_loss_ = MSELoss(mlp, targets);
+
   return 0;
 }
