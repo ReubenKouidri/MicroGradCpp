e195ec7db8819bffdd09c6d659d88cf1c60a9d9d
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-12-19 09:46:21 +0000

    fixed bug in losses

--- include/losses.hpp
+++ include/losses.hpp
@@ -16,6 +16,9 @@ public:
 protected:
   MLP<T> network_;
   double learning_rate_;
+  Value<T> value_ {static_cast<T>(0)};
+  static constexpr T eps_ = 1e-7;
+  static constexpr T alpha_ = 10;
 
 public:
   explicit Loss(const MLP<T>& network, const double learning_rate)
@@ -26,20 +29,31 @@ public:
   Loss& operator=(const Loss& other) = delete;
   Loss& operator=(Loss&& other) = delete;
 
-  virtual Value<T> compute_loss(const input_type& input, const target_type& target) = 0;
-  virtual Value<T> compute_loss(const batched_input_type& batched_input, const batched_target_type& batched_target) {
-    Value<T> total_loss = Value<T>(0.0);
+  virtual void compute_loss(const input_type& input,
+                            const target_type& target) = 0;
+  virtual void compute_loss(const batched_input_type& batched_input,
+                            const batched_target_type& batched_target) {
     for (size_t i = 0; i < batched_input.size(); ++i) {
-      total_loss += compute_loss(batched_input[i], batched_target[i]);
+      compute_loss(batched_input[i], batched_target[i]);
     }
-    return total_loss / static_cast<T>(batched_input.size());
+    value_ /= static_cast<T>(batched_input.size());
   }
 
   virtual void step() const { network_.step(learning_rate_); }
   virtual void zero_grad() const { network_.zero_grad(); }
+  virtual void zero() { value_ = Value(static_cast<T>(0)); }
+  virtual void clamp(Output<T>& output) {
+    for (auto& val : output) {
+      val.get_data() = std::clamp(val.get_data(),
+                                  this->eps_, 1 - this->eps_);
+    }
+  }
+  virtual T get() { return value_.get_data(); }
+  virtual void backward() { value_.backward(); }
+
 };
 
-/*==================================================================================================================*/
+/*============================================================================*/
 template<class T>
 class SparseCCELoss final : public Loss<T, uint8_t> {
 public:
@@ -50,21 +64,19 @@ public:
   using typename           Loss::batched_target_type;
 
   using Loss::Loss;  // Inherit constructor
+  using Loss::compute_loss;
 
   // Implement compute_loss for single input
-  Value<T> compute_loss(const input_type& input, const target_type& target) override {
-    auto outputs = this->network_(input); // network output is a softmax vector
-    Value<T> loss = -vlog(outputs[target]);
-    return loss;
+  void compute_loss(const input_type& input,
+                    const target_type& target) override {
+    auto outputs = this->network_(input);
+    this->clamp(outputs);
+    this->value_ -= vlog(outputs[target]);
   }
-
-  // Use the base class implementation for batched input
-  using Loss::compute_loss;
 };
 
-/*==================================================================================================================*/
-
-/* specialisation for single input vector where target is ohe, e.g. {0, 1, 0, ..., 0} */
+/*============================================================================*/
+/* target ohe, e.g. {0,...,1, 0} */
 template<class T>
 class CCELoss final: public Loss<T, std::vector<uint8_t>> {
 public:
@@ -76,19 +88,17 @@ public:
 
   using Loss::Loss;  // Inherit constructor
 
-  Value<T> compute_loss(const input_type& input,
-                        const target_type& target) override {
-    auto output = this->network_(input); // network output is a softmax vector
-    Value<T> loss = Value<T>(0.0);
+  void compute_loss(const input_type& input,
+                    const target_type& target) override {
+    auto output = this->network_(input);
     for (size_t i = 0; i < output.size(); ++i) {
-      loss += -vlog(output[i]) * Value<T>(target[i]);
+      this->value_ += -vlog(output[i]) * Value<T>(target[i]);
     }
-    return loss;
   }
   using Loss::compute_loss;
 };
 
-/*==================================================================================================================*/
+/*============================================================================*/
 /* specialisation for single input, sparse target */
 
 template<class T>
@@ -102,18 +112,18 @@ public:
 
   using Loss::Loss;  // Inherit constructor
 
-  Value<T> compute_loss(const input_type& input,
-                        const target_type& target) override {
+  void compute_loss(const input_type& input,
+                    const target_type& target) override {
     auto output = this->network_(input);
-    auto loss = Value<T>(0.0);
+    this->clamp(output);
+
     for (size_t i = 0; i < output.size(); i++) {
       if (i == static_cast<size_t>(target))
-        loss += pow(output[i] - 1.0, static_cast<T>(2));
+        this->value_ += pow(output[i] - 1.0, static_cast<T>(2));
       else
-        loss += pow(output[i], static_cast<T>(2));
+        this->value_ += pow(output[i], static_cast<T>(2));
     }
-    loss /= output.size();
-    return loss;
+    this->value_ /= output.size();
   }
   using Loss::compute_loss;
 };
