21bf82c3385f00139fce9ca0d8036c47fec9d46b
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-01-02 17:09:44 +0000

    updated loss function tests

--- tests/loss_functions_test.cpp
+++ tests/loss_functions_test.cpp
@@ -2,31 +2,48 @@
 #include "../include/components.hpp"
 #include "../include/losses.hpp"
 #include "../include/optimiser.hpp"
+#include "trainer.hpp"
 
 class LossFunctionsTest : public testing::Test {
-protected:
+ protected:
   void SetUp() override {
   }
 
-  const std::vector<double> input {1.0, 0.0, 1.0};
-  static constexpr uint8_t tgt = 0;
+  const std::vector<double> input0{1.0, 0.0, 0.0};
+  const std::vector<double> input1{0.0, 1.0, 0.0};
+  const std::vector<double> input2{0.0, 0.0, 1.0};
 
-  const std::vector<uint8_t> categorical_tgt {1, 0};
-  const std::vector<std::vector<uint8_t>> batched_categorical_tgt {
-    categorical_tgt, categorical_tgt, categorical_tgt};
+  static constexpr uint8_t tgt0 = 0;
+  static constexpr uint8_t tgt1 = 1;
+  static constexpr uint8_t tgt2 = 2;
 
-  std::vector<std::vector<double>> batched_input {input, input, input};
-  std::vector<uint8_t> batched_tgt {tgt, tgt, tgt};
+  std::vector<uint8_t> batched_sparse_tgt{tgt0, tgt1, tgt2};
+
+  const std::vector<uint8_t> categorical_tgt0{1, 0, 0};
+  const std::vector<uint8_t> categorical_tgt1{0, 1, 0};
+  const std::vector<uint8_t> categorical_tgt2{0, 0, 1};
+
+  const std::vector<std::vector<uint8_t>> batched_categorical_tgt{
+      categorical_tgt0,
+      categorical_tgt1,
+      categorical_tgt2};
+
+  std::vector<std::vector<double>> batched_input{input0, input1, input2};
 
   static constexpr double learning_rate = 0.05;
   static constexpr size_t epochs = 25;
 
-  const Layer<double> layer0 {3, 3, UnaryOp::relu};
-  const Layer<double> layer1 {3, 2, UnaryOp::softmax};
-  MLP<double> model {{layer0, layer1}};
-  SparseCCELoss<double> sparse_cce_loss {&model};
-  CCELoss<double> cce_loss {&model};
-  MSELoss<double> mse_loss {&model};
+  const MLP<double> model{{
+                              Layer<double>(3, 3, UnaryOp::relu),
+                              Layer<double>(3, 3, UnaryOp::softmax)
+                          }};
+  const std::shared_ptr<const MLP<double>>
+      mp = std::make_shared<MLP<double>>(model);
+
+  SparseCCELoss<double> sparse_cce_loss{mp};
+  Adam<double> adam{mp, learning_rate};
+  CCELoss<double> cce_loss{mp};
+  MSELoss<double> mse_loss{mp};
 };
 
 TEST_F(LossFunctionsTest, test_sparse_cce) {
@@ -34,35 +51,35 @@ TEST_F(LossFunctionsTest, test_sparse_cce) {
   std::cout << "======= TESTING SPARSE CCE LOSS ========\n";
   std::cout << "========================================\n";
 
-  train_model(model,
-                input,
-                tgt,
-                sparse_cce_loss,
-                learning_rate,
-                epochs);
+  train_model(mp,
+              input0,
+              tgt0,
+              sparse_cce_loss,
+              adam,
+              epochs);
 }
 
 TEST_F(LossFunctionsTest, test_sparse_cce_batched) {
   std::cout << "==============================================\n";
   std::cout << "=======TESTING BATCHED SPARSE CCE LOSS========\n";
   std::cout << "==============================================\n";
-  train_model(model,
-                batched_input,
-                batched_tgt,
-                sparse_cce_loss,
-                learning_rate,
-                epochs);
+  train_model(mp,
+              batched_input,
+              batched_sparse_tgt,
+              sparse_cce_loss,
+              adam,
+              epochs);
 }
 
 TEST_F(LossFunctionsTest, test_cce) {
   std::cout << "============================================\n";
   std::cout << "============= TESTING CCE LOSS =============\n";
   std::cout << "============================================\n";
-  train_model(model,
-              input,
-              categorical_tgt,
+  train_model(mp,
+              input0,
+              categorical_tgt0,
               cce_loss,
-              learning_rate,
+              adam,
               epochs);
 }
 
@@ -70,34 +87,34 @@ TEST_F(LossFunctionsTest, test_batched_cce) {
   std::cout << "=============================================\n";
   std::cout << "========= TESTING BATCHED CCE LOSS ==========\n";
   std::cout << "=============================================\n";
-  train_model(model,
-                 batched_input,
-                 batched_categorical_tgt,
-                 cce_loss,
-                 learning_rate,
-                 epochs);
+  train_model(mp,
+              batched_input,
+              batched_categorical_tgt,
+              cce_loss,
+              adam,
+              epochs);
 }
 
 TEST_F(LossFunctionsTest, test_mse) {
   std::cout << "======================================\n";
   std::cout << "==========TESTING MSE LOSS============\n";
   std::cout << "======================================\n";
-  train_model(model,
-                input,
-                tgt,
-                mse_loss,
-                learning_rate,
-                epochs);
+  train_model(mp,
+              input0,
+              tgt0,
+              mse_loss,
+              adam,
+              epochs);
 }
 
 TEST_F(LossFunctionsTest, test_batched_mse) {
   std::cout << "==============================================\n";
   std::cout << "==========TESTING BATCHED MSE LOSS============\n";
   std::cout << "==============================================\n";
-  train_model(model,
-                batched_input,
-                batched_tgt,
-                mse_loss,
-                learning_rate,
-                epochs);
+  train_model(mp,
+              batched_input,
+              batched_sparse_tgt,
+              mse_loss,
+              adam,
+              epochs);
 }
